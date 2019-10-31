666d276a314bf7d2cf8868b2be38d561200d1ec2
ousmane.ndiaye.dev@gmail.com <ousmane.ndiaye.dev@gmail.com>
2019-10-31 16:10:31 +0000

    my version 2 of the project groupe galima

---
+++ vue/emplois.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+Emploi du temps
+
+Emploi du temps classe de 6éme
+Emploi du temps classe de 5éme
+Emploi du temps classe de 4éme
+Emploi du temps classe de 3éme
+Emploi du temps classe de 2nde S
+Emploi du temps classe de 1ére S
+Emploi du temps classe de Terminale S
+
+
+</body>
+</html>
---
+++ vue/presentation.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>presentation</title>
+    <link rel="stylesheet" href="../css/style1.css" />
+    <link rel="stylesheet" href="../css/style2.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Radley&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<body>
+   
+<div class="container">
+<header>
+    <div class="barMenu">
+        <ul id="barMenu">
+            <li><a href="index.html" class="barLien">accueil</a></li>
+            <li><a href="vue/presentation.php" class="barLien">A propos</a></li>
+            <li><a href="vue/admissins.php" class="barLien">ADMISSINS</a></li>
+            <li><a href="vue/inscriptions.php" class="barLien">inscriptions</a></li>
+            <li><a href="vue/emplois.php" class="barLien">emplois du temps</a></li>
+            <li><a href="vue/contacts.php" class="barLien">contacts</a></li>
+
+            
+        </ul>
+    </div>
+</header>
+
+    <div>
+  <h2>
+      DESCRIPTIF DE L’INSTITUTION ASKHABOU RASSOULOULAHI POUR L’EDUCATION ET LA PROMOTION HUMAINE
+  </h2>
+    </div>
+<div class="devpress">
+<div class="devpress1">
+<img src="../images/IMG-20190319-WA0000.jpg" height="400px" width="100%" >
+</div>
+<div class="devpress2">
+<H1>I- Présentation</H1>
+
+Bref aperçu sur un enseignement en continuité Cette présentation, vise entre autre, à donner un aperçu global sur l’historique, l’encrage et la continuité de l’enseignement arabo-islamique et la mémorisation du Saint Coran dans l’institution DAROUL IMANE. Le fondateur de l’institution DAROUL IMANE, Dr Mohamed Habiboullah SY, est le fils du célèbre jurisconsulte et le spécialiste en science coranique, Thierno Mouhamed Thierno Hamidou SY, de Fouta dans le village de Diattar aux alentours de Podor qui se situe à 500 kilomètres de Dakar. Il est en même temps le petit-fils de l’éminent érudit et la grande figure de L’Islam, Cheikh Thierno Hamidou SY de Diattar, connu à Fouta et dans les pays limitrophes du Sénégal, titulaire du célèbre chair des sciences du coran et la législation islamique où 15 des petits-fils de Cheikh El Hadj Omar Foutiyou TALL ont fait leurs études, au premier rang desquels, l’ancien Calife de la famille omarienne du Sénégal, Thierno Mountaga Tall, qui était resté 10 ans auprès du Cheikh Thierno Hamidou à Diattar ainsi que le calife de la famille omarienne au Mali, Cheikh Thierno Madani Tall, qui était resté disciple du Cheikh pendant 12 ans d’initiation et d’apprentissage avec son maitre. C’est dans cet environnement approprié et bienveillant, que naquit le Fondateur de l’institution Daroul Imane où il a fait ses premières humanités: l’apprentissage du Coran, la langue arabe et les sciences de la législation islamique, avant de voyager à l’étranger pour compléter ses études et sa formation, ce qui fait de son action et les efforts consentis comme une continuité de l’œuvre de ses parents et celle de ses arrières grands parents. Que le Tout Puissant leur accorde sa miséricorde ainsi que l’ensemble des musulmans disparus.
+<H1>II- Dénomination et présentation</H1>
+
+L’Institution Daroul Imane pour l’éducation et la promotion humaine, tire son appellation d’un des noms désignant la ville de Médine où le fondateur a séjourné 7 ans pour ses études. C’est une institution d’enseignement, d’éducation sociale et islamique. Elle s’occupe à la fois de la mémorisation du Saint Coran et de l’éducation islamique des enfants musulmans à la lumière du Coran et de la Sunna.
+<H1>III- Siège</H1>
+
+Le siège de l’Institution se trouve à Sicap Mbao, la cité « Daroul Imane » en face de la LGI, plle n° 105. Téléphone : 33 854 80 80 Fax : 33 854 50 54, e-mail : eduman2020@gmail.com
+<H1>IV- Création et autorisation</H1>
+
+L’Institution Daroul Imane a été créée en 1997 (1417H) à Dakar. Elle a été reconnue officiellement à la fois par le Ministère de l’intérieur sous le n° 12197 et par le Ministère de l’Education Nationale sous le n° 4801 pour ouvrir les trois cycles : l’enseignement élémentaire, moyen et secondaire. Elle est reconnue ONG agrément n° 03150.
+<H1>V- Objectifs</H1>
+
+• Faire du citoyen un homme vertueux et fier de sa foi.
+• Former les enfants des deux sexes pour la mémorisation du Saint Coran en régime d’internat;
+• Etre attentif à l’éducation post-mémorisation du Coran par une formation scientifique et académique ;
+• Enseigner les langues arabe, française et anglaise
+• Donner aux enfants une éducation islamique équilibrée.
+• S’intéresser aux technologies modernes telles que l’informatique et l’internet.
+• Diffuser la culture du juste milieu en toute chose.
+• Prendre en charge la scolarité de certains orphelins et orphelines nécessiteux ;
+• Organiser des séminaires de formations scientifiques et pédagogiques au profit des enseignants ;
+• Distribuer des aides aux nécessiteux pendant le ramadan, les fêtes et la rentrée scolaire ;
+• Construire des mosquées, des écoles, forer des puits et organiser des caravanes médicales ;
+• Nouer des relations de coopération avec les autorités locales pour servir la société ;
+<H1>VI- Composantes de l’Institution</H1>
+
+1/ Le jardin d’enfants islamique
+Créé en 2000, le jardin accueille des enfants âgés de 3 à 6 ans qui sont initiés à l’apprentissage de courtes sourates, des invocations du prophète (p.s.l), des contes, des compétences pédagogiques, et des chansons religieuses en langue arabe et française. A cela, s’ajoutent les jeux, les activités sportives et des sorties pédagogiques dans des lieux administratifs, touristiques et culturels.
+Le droit d’inscription est de 15.000F et 10.000f la mensualité.
+2/ L’institut coranique moderne
+Fondé en 1997, l’Institut moderne pour l’enseignement du Coran, en régime d’internat, accueille des filles et des garçons qui s’adonnent entièrement à la mémorisation du Saint Coran. En dehors de la mémorisation du coran, les élèves reçoivent des cours en langue arabe et française et en plus l’éducation islamique. L’Institut prend également en charge leur hébergement, leur restauration ainsi que leur protection sanitaire, sociale et sécuritaire. Les filles sont séparées des garçons au niveau de leur hébergement et de leur enseignement. Il est réservé à chaque sexe un logement, des salles de cours, des toilettes, et des escaliers loin de toute promiscuité.
+Le montant de l’inscription est de 75.000F et 40.000Fpar mois représentant les frais de scolarité, d’hébergement, de restauration, de sécurité et de soins sanitaires. Il est bon de noter qu’après la phase de mémorisation du Coran, certains élèves y restent pour continuer leurs études élémentaires, moyennes et secondaires au terme desquelles ils font une demande d’inscription dans les universités intérieures et extérieures. A ce titre, deux promotions sont déjà sorties de l’institut avec une Licence et continuent leurs études supérieures en Master et en Doctorat. De même, l’Institut octroie des diplômes de maitrise et d’enseigner le Coran appelés ( ijaza) en trois riwaya : warche, qaloun et hafs avec une chaine de transmission continue jusqu’au prophète (psl). Le Directeur Général, l’équipe pédagogique, les encadreurs, le médecin et les femmes de ménage sont tous logés avec les élèves à l’institut pour un meilleur travail et de suivi 24H/24H. L’Institut dispose de 28 chambres pour le logement des élèves et de deux grandes salles pour les cercles coraniques d’apprentissage, 18 toilettes. A cela s’ajoutent le logement du Directeur et celui des encadreurs.
+3/ Le Complexe Educatif Islamique Daroul Imane
+C’est une extension de Daroul Imane créé en 2000 à Diack Sao puis transféré à Sicap Mbao. Il a été inauguré le 27 Janvier 2012 par le Professeur Kalidou Diallo, ex-ministre de l’éducation, par délégation du Président Abdoulaye Wade. Le complexe comprend trois cycles : le cycle élémentaire, le cycle moyen et le cycle secondaire. Le droit d’inscription est de 10000F et la mensualité varie selon les niveaux( de 8000f à 15000f). Le complexe enseigne le programme franco-arabe du Ministère de l’éducation nationale sous la supervision de l’IEF de Thiaroye.
+Il comprend :
+
+• Une grande mosquée d’une superficie de 600m2
+• 1 salle de prières pour les femmes à l’étage, d’une superficie de 150m2
+• 1 morgue pour la conservation et le lavage mortuaire
+• 4 magasins à louer, dont les recettes sont destinées à l’entretien de la morgue et de la mosquée
+• 33 salles de classe et 8 bureaux administratifs
+• Une salle de conférences et de séminaires d’une capacité de 250 places, ouverte gratuitement à tous les musulmans.
+Voici quelques associations et établissements qui y ont une fois tenu leurs activités :
+1- La fédération nationale des écoles coraniques
+2- L’Association des cadres musulmans
+3- L’Association des ibadourahman
+4- Le Mouvement al-falah
+5- La coordination des musulmans de Dakar
+6- L’Institut Dar al-arqam pour l’enseignement du saint coran de Fass touré
+7- Le Parti CAARLEENEN de Amsata Sow Sidibé
+8- L’Association des jeunes de Sicap Mbao
+9- Le Mouvement des femmes de Sicap Mbao
+10- L’Association des ressortissants de Donaye de Podor
+11- L’université islamique de Médine (séminaire de formation en 2013)
+12- L’Association de la renaissance du patrimoine et de l’expansion de la culture arabe au Sénégal
+<H1>VII- Conseil consultatif</H1>
+
+L’Institution dispose d’un conseil consultatif composé de 11 membres dont des érudits, des académiciens, des experts en éducation, en administration et en droit dans le but de recueillir leurs opinions sur les questions pédagogiques, éducatives, méthodologiques et administratives.
+<H1>VIII- Ressources financières</H1>
+
+L’institution tire ses ressources financières des/ du / de la/ de l’ :
+1- Frais de scolarité.
+2- Soutien des parents d’élèves.
+3- Donation de certains commerçants et d’hommes d’affaire via les parents d’élèves.
+4- Ambassade du Royaume d’Arabie Saoudite à Dakar.
+5- Soutien du Ministère de l’Education Nationale.
+6- Soutien de l’Association de la renaissance du patrimoine et de l’expansion de la culture arabe au Sénégal.
+
+
+<div>
+
+
+<H1>Réalisations:<H1>
+
+1- mémorisation du Saint Coran par plus de 400 élèves.
+2- Application du programme du Ministère de l’Education nationale en enseignement Franco-Arabe dans les 4 cycles : préscolaire, élémentaire, moyen et secondaire.
+3- Inscription des bacheliers dans les universités( Cheikh Anta Diop de Dakar ,Gaston Berger de Saint-louis , Oumou al-quraa (Mecque), l’université al-imam de Riyad, l’université al-amira Noura des femmes à Riyad, l’université Mouhamed V de Rabat, l’université de Marrakech, l’université de Koweit, l’université d’Istanbul (Turquie), l’université internationale d’Afrique (Soudan) et l’université internationale islamique de Malaisie.
+Il est à noter que l’année dernière nous avions obtenu 26 bourses d’excellence de ces universités.
+4 – Attribution de notre établissement comme waqf pour assurer la pérennité de son action et le renouveau de son expression.
+5/ Existence d’une équipe pédagogique, compétente, qualifiée et formée dans les écoles de formation du pays.
+6/ Participation à la politique nationale de création d’emplois pour les citoyens. Nous avons 85 employés.
+7/- Résolution au paiement des impôts annuels au Ministère des finances et versement de cotisations à la caisse de sécurité sociale et à l’IPRES.
+8/-Etablissement d’un climat de confiance chez les parents d’élèves envers l’institution qui a donné lieu à une forte affluence, à tel point que la liste d’attente est devenue très longue.
+9/- utilisation des nouvelles technologies dans l’administration scolaire et à la recherche scientifique en mettant à la disposition des élèves une salle informatique et une bibliothèque.
+10/- ouverture d’une bibliothèque scolaire où se forment les élèves du secondaire en recherches, sous la supervision du professeur encadreur.
+11/ Fourniture de jeux et d’équipements scolaires dont ont besoin les enfants du jardin.
+12/ Solidification des relations avec les autorités de la localité et les différents agents de la collectivité locale.
+13/ Organisation des dizaines de séminaires pour la formation de l’équipe pédagogique et la modernisation de leurs outils de travail.
+14/ Reconnaissance de l’Institution par l’Etat via récépissé du Ministère de l’intérieur, autorisation du Ministère de l’Education Nationale et ONG au Sénégal
+15/ Dénomination officielle du quartier de l’institution (cité daroul imane) par l’ex-maire vu son rôle dans la localité.
+16/ Partenariat avec l’IFE de l’UCAD pour organiser des cours intensifs afin de relever le niveau de la langue française chez les enseignants et fonctionnaires arabophones de l’institut.
+17-Inscription de plus de 1300 élèves dans nos établissements.
+ Réalisations dans le social:
+
+1- Distribution annuelle de 24 tonnes de dattes, depuis 10 ans, dans des mosquées et des écoles coraniques, aux nécessiteux sans distinction entre les musulmans
+2- Parrainage des orphelines et orphelins en collaboration avec l’Association de la renaissance du patrimoine et de l’expansion de la culture arabe au Sénégal.
+3- Distribution des fournitures scolaires et des habits aux élèves des écoles publiques dans les banlieues dakaroises.
+4- Formation d’un comité social permanent dans la mosquée pour satisfaire les besoins des populations et résoudre leurs problèmes chaque semaine après la prière de vendredi.
+5- Inscription gratuite de 5% des enfants des démunis.
+Projets futurs:
+
+1- Achat d’un terrain à Diass à côté du nouvel aéroport pour construire un grand complexe éducatif qui englobera le cycle primaire jusqu’en doctorat.
+2- Accord de partenariat académique avec l’université internationale d’Afrique pour ouvrir une faculté d’éducation à partir de l’année 2016 .
+3- Ouverture des annexes dans les localités, comme Tarédji, département de Podor, où nous avons acheté un terrain afin de construire un institut islamique pour les fils de la localité.
+
+<H1>Galerie<H1>
+
+</div>
+
+<footer>
+           <div class="divfooter">
+             <div class="divfooter1">
+               <P class="imgfloatleft">
+                <H2>PRÉSENTATION DE ASKHABOU RASSOULOULAHI</H2>
+               <img src="images/0_fasade.jpg" width="30%" height="150" style="float: left;"> 
+               Bref aperçu sur un enseignement en continuité Cette présentation, vise entre autre, à donner un aperçu global sur l’historique, l’encrage et la continuité de l’enseignement arabo-islamique
+                et la mémorisation du Saint Coran dans l’institution ASKHABOU RASSOULOULAHI...<a href="#">Lir la suite</a> </div>  
+             <div class="divfooter2"> 
+                <P class="imgfloatleft">
+                    <h2>PRÉSENTATION DU GROUPE ASKHABOU RASSOULOULAHI</h2>
+                    <img src="images/0_groupe.png" width="30%" height="150" style="float: left;">ISI est un institut délocalisé en Afrique et à l'intérieur du Sénégal. Il offre un ensemble d'écoles et de centres. </div>  
+              <div class="divfooter3">
+                    <P class="imgfloatleft">
+                        <h2>MEDIATHEQUE DE L'INSTITUT ASKHABOU RASSOULOULAHI</h2>
+
+
+                        <li style="float: left;"><FONT SIZE=5><a href="#">Videotehque groupe </FONT></a></li></BR>
+                        <li style="float: left;"><FONT SIZE=5><a href="#">Nos Galeries photo</a></li><BR>
+                        <li style="float: left;"><FONT SIZE=5><a href="#">Nos temignages</FONT></a></li><BR>
+                        <li style="float: left;"><FONT SIZE=5><a href="#">Anciens eleves</FONT></a></li><BR>
+                </div>  
+            </div>  
+            <br><br>
+              <center><hr width="70%" style="color: honeydew;"></center>
+            <br>   
+           &COPY; copyright ASKHABOU RASSOULOULAHI
+       </footer>
+    </div>      
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</html>
